2441519be3df898d04c20a30d79db42b6a50b90e
yaratul2005 <ratul41g@gmail.com>
2026-02-02 10:35:46 +0600

    feat: Implement admin page builder for creating, editing, and deleting custom pages with TinyMCE and SEO fields.

--- admin/pages.php
+++ admin/pages.php
@@ -59,7 +59,7 @@ try {
     <title>Page Builder - CMS</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <!-- TinyMCE CDN -->
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/phkw2r1pek7jx50e7gutbshu6gvn9ekjvttj8ilctjm1iiyg/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
       tinymce.init({
         selector: '#editor',
